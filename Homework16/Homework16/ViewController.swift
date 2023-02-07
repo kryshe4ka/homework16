f6386359e176661d0acc3e8c61ac8331cc6fd161
kryshe4ka <kryshe4ka@gmail.com>
2023-02-07 18:54:32 +0400

    Create progect

---
+++ Homework16/Homework16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework16
+//
+//  Created by Liza Kryshkovskaya on 7.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
